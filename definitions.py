ab21b341e886687f8c0ec916174fb6d9ceb44bb6
fishzint <95106735+fishzint@users.noreply.github.com>
2022-08-11 00:00:25 -0400

    Add files via upload

--- definitions.py
+++ definitions.py
@@ -3,27 +3,27 @@ import os
 ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 settings = {
-    "capmonsterKey": "d54b31eb7fa48a18023190487c975ce8",
-    "botToken": "MTAwMTgxNTMwMzMzODA2NjAyMQ.G56iel.dRH18IZL1EujVilzTo67AR7_Jk9wdqKHA758XQ",
-    "guildID": 974347655775916072,
+    "capmonsterKey": "",
+    "botToken": "",
+    "guildID": ,
     "botAdminId": [
-        "920147549468385361",
-        "988620985022427136"
+        "",
+        ""
     ],
     "botWhitelistedId": [
-        "920147549468385361",
-        "988620985022427136,"
+        "",
+        ","
        
     ]
 }
 
-CAPMONSTER_API_KEY = "d54b31eb7fa48a18023190487c975ce8"
+CAPMONSTER_API_KEY = ""
 
-GUILD_ID = 1001808507026284636
-BOT_TOKEN = "MTAwMTgxNTMwMzMzODA2NjAyMQ.G56iel.dRH18IZL1EujVilzTo67AR7_Jk9wdqKHA758XQ"
-BOT_OWNER_IDS = [920147549468385361]
+GUILD_ID = 
+BOT_TOKEN = ""
+BOT_OWNER_IDS = []
 
-LOG_CHANNEL = 1001809507099353109
+LOG_CHANNEL = 
 
 X_SUPER_PROPERTIES = "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImVuLUdCIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzk1LjAuNDYzOC41NCBTYWZhcmkvNTM3LjM2IiwiYnJvd3Nlcl92ZXJzaW9uIjoiOTUuMC40NjM4LjU0Iiwib3NfdmVyc2lvbiI6IjEwIiwicmVmZXJyZXIiOiIiLCJyZWZlcnJpbmdfZG9tYWluIjoiIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjEwMjExMywiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbH0="
 
